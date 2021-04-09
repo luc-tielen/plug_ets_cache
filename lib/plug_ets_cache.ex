0b50907b741324e7d45fb409c77559ce361cb92e
Luc Tielen <luc.tielen@kabisa.nl>
2021-04-09 11:12:11 +0200

    Allow turning off ttl check

--- lib/plug_ets_cache.ex
+++ lib/plug_ets_cache.ex
@@ -8,16 +8,21 @@ defmodule PlugEtsCache do
   def start(_type, _args) do
     import Supervisor.Spec, warn: false
 
-    children = [
-      worker(ConCache, [
-        [
-          name: app_env(:db_name, :ets_cache),
-          ttl_check_interval: :timer.seconds(app_env(:ttl_check, 60)),
-          global_ttl: :timer.seconds(app_env(:ttl, 300))
-        ]
-      ])
-    ]
+    ttl_check = app_env(:ttl_check, 60)
+    con_cache_opts = if ttl_check do
+      [
+        name: app_env(:db_name, :ets_cache),
+        ttl_check_interval: :timer.seconds(ttl_check),
+        global_ttl: :timer.seconds(app_env(:ttl, 300))
+      ]
+    else
+      [
+        name: app_env(:db_name, :ets_cache),
+        ttl_check_interval: false
+      ]
+    end
 
+    children = [{ConCache, con_cache_opts}]
     opts = [strategy: :one_for_one, name: PlugEtsCache]
     Supervisor.start_link(children, opts)
   end
